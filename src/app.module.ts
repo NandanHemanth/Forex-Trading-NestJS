95644b07af3f14cca5a6f5bbe47d6b31223d737f
Nandan H <theuniverseisflat101@gmail.com>
2024-04-28 19:09:58 +0530

    Adding comments to main.ts and app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+// Importing Functions & modules 
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { AccountsModule } from './accounts/accounts.module';
@@ -6,9 +7,10 @@ import { FxRatesController } from './fx-rates.controller';
 import { FxConversionService } from './fx-conversion.service';
 import { FxConversionController } from './fx-conversion.controller';
 
+// Module decorator to define all the module metadata
 @Module({
-  imports: [AccountsModule, HttpModule],
-  controllers: [FxRatesController, FxConversionController],
-  providers: [FxRatesService, FxConversionService],
+  imports: [AccountsModule, HttpModule], // Modules that provide metadata used to organise application structure
+  controllers: [FxRatesController, FxConversionController], // Controllers used for  handling incoming requests, outgoing responses and route management
+  providers: [FxRatesService, FxConversionService], // Service which handle the core functionality of the incoming requests
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+// Importing Modules and necessary libraries
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Async Main function which is the entry point for the application on port 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
