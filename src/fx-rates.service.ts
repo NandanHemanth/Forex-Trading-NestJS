dadcc7e176f4c0529bb8b33d951860325ec79f8f
Nandan H <theuniverseisflat101@gmail.com>
2024-04-28 16:24:51 +0530

    Forex-Trading-System wokring

--- src/fx-rates.service.ts
+++ src/fx-rates.service.ts
@@ -4,30 +4,43 @@ import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class FxRatesService {
-  private apiKey = 'ARQRE1IJ0BLY95YX';
-  private rates: { [key: string]: { rate: number; expiresAt: number } } = {};
+  private apiKey = 'GF4OH829SV9XJLIJ';
+  private rates: { [currency: string]: { rate: number; expiresAt: number } } = {};
 
   constructor(private httpService: HttpService) {
     this.fetchRates();
-    setInterval(() => this.fetchRates(), 30000); // Fetch new rates every 30 seconds
+    setInterval(() => this.fetchRates(), 300000); // Fetch new rates every 30 seconds
   }
 
   private async fetchRates() {
-    const response = await lastValueFrom(
-      this.httpService.get(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&apikey=${this.apiKey}`),
-    );
-    const data = response.data['Realtime Currency Exchange Rate'];
-    for (const [currency, rate] of Object.entries(data['5. Exchange Rate'])) {
-      this.rates[currency] = {
-        rate: parseFloat(rate['5. Exchange Rate']),
-        expiresAt: Date.now() + 30000, // Expire in 30 seconds
+    try {
+      const response = await lastValueFrom(
+        this.httpService.get(`https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=USD&to_symbol=EUR&apikey=${this.apiKey}`),
+      );
+    //   console.log(response.data)
+
+      const data = response.data['Time Series FX (Daily)'];
+      if (!data) {
+        throw new Error('Unexpected response format: Time Series FX (Monthly) not found');
+      }
+
+      const latestRate = data[Object.keys(data)[0]];
+      const rate = parseFloat(latestRate['4. close']); 
+
+      const expiresAt = Date.now() + 300000; // Expire in 30 seconds
+
+      this.rates['EUR'] = {
+        rate,
+        expiresAt,
       };
+    } catch (error) {
+      console.error('Error fetching rates:', error);
     }
   }
 
   getRates() {
-    const quoteId = Math.floor(Math.random() * 1000000).toString(); // Generate a random quoteId
+    const quoteId = Math.floor(Math.random() * 100000).toString(); // Generate a random quoteId
     const expiresAt = Math.max(...Object.values(this.rates).map((rate) => rate.expiresAt));
     return { quoteId, rates: this.rates, expiresAt };
   }
-} 
+}
